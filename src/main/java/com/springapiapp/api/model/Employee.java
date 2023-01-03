d532f1a509b70e244e6087f3cbeb2871025bb98c
Alex Gitari <gitarimurithialex@gmail.com>
2023-01-03 17:05:04 +0300

    added employee entity

---
+++ src/main/java/com/springapiapp/api/model/Employee.java
@@ -0,0 +1,33 @@
+package com.springapiapp.api.model;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "created_at", nullable = true)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = true)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
